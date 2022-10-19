3b22c74e7322504b9d19888070626db9c3bc072d
unknown <david.otalora-b@labinfo.is.escuelaing.edu.co>
2022-10-19 18:13:13 -0500

    Seure URL reader and keystores added

--- src/main/java/co/edu/escuelaing/sparkdockerdemolive/SecureURLReader.java
+++ src/main/java/co/edu/escuelaing/sparkdockerdemolive/SecureURLReader.java
@@ -1,2 +1,76 @@
-package co.edu.escuelaing.sparkdockerdemolive;public class SecureURLReader {
+package co.edu.escuelaing.sparkdockerdemolive;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.net.*;
+import java.io.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+public class SecureURLReader {
+
+
+    public static void main(String[] args) throws Exception {
+        // Create a file and a password representation
+        File trustStoreFile = new File("keystores/ecikeystore.p12");
+        char[] trustStorePassword = "12345678".toCharArray();
+        // Load the trust store, the default type is "pkcs12", the alternative is "jks"
+        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        trustStore.load(new FileInputStream(trustStoreFile), trustStorePassword);
+        // Get the singleton instance of the TrustManagerFactory
+        TrustManagerFactory tmf = TrustManagerFactory
+                .getInstance(TrustManagerFactory.getDefaultAlgorithm());
+
+        // Itit the TrustManagerFactory using the truststore object
+        tmf.init(trustStore);
+        //Set the default global SSLContext so all the connections will use it
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, tmf.getTrustManagers(), null);
+        SSLContext.setDefault(sslContext);
+        // We can now read this URL
+        readURL("https://localhost:5000/hello");
+        // This one can't be read because the Java default truststore has been
+        // changed.
+        readURL("https://www.google.com");
+    }
+
+    private static void readURL(String url) throws IOException {
+        try {
+            URL siteURL = new URL(url);
+            URLConnection urlConnection = siteURL.openConnection();
+            Map<String, List<String>> headers = urlConnection.getHeaderFields();
+            Set<Map.Entry<String, List<String>>> entrySet = headers.entrySet();
+            for (Map.Entry<String, List<String>> entry : entrySet) {
+                String headerName = entry.getKey();
+                if (headerName != null) {
+                    System.out.print(headerName + ":");
+                }
+                List<String> headerValues = entry.getValue();
+                for (String value : headerValues) {
+                    System.out.print(value);
+                }
+                System.out.println("");
+            }
+            System.out.println("-------message-body------");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String inputLine = null;
+            while ((inputLine = reader.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/main/java/co/edu/escuelaing/sparkdockerdemolive/SparkWebServer.java
+++ src/main/java/co/edu/escuelaing/sparkdockerdemolive/SparkWebServer.java
@@ -1,7 +1,7 @@
 package co.edu.escuelaing.sparkdockerdemolive;
 
-import static spark.Spark.get;
-import static spark.Spark.port;
+import static spark.Spark.*;
+
 //co.edu.escuelaing.sparkdockerdemolive.SparkWebServer.java
 public class SparkWebServer {
 
@@ -9,6 +9,8 @@ public class SparkWebServer {
     public static void main(String... args){
         port(getPort());
         System.out.println(getPort());
+        //API: secure(keystoreFilePath, keystorePassword, truststoreFilePath, truststorePassword);
+        secure("keystores/ecikeystore.p12", "12345678", null, null);
         get("hello", (req,res) -> "Hello Spark!");
     }
 
@@ -16,6 +18,8 @@ public class SparkWebServer {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
         }
-        return 4567;
+        return 5000;
     }
 }
+
+//keytool -genkeypair -alias ecikeypair -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore ecikeystore.p12 -validity 3650
